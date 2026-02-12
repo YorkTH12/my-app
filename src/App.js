93388c301c9528477fdb668fd266e2b8dbb8f165
YorkTH12 <148939113+YorkTH12@users.noreply.github.com>
2026-02-12 10:02:07 +0700

    NavLink

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 import {userContext} from "./context"
 import Header2 from './context-header2';
 import Content2 from './context-content2'
+import Router2 from './router2'
 
-function App() {
-  let [user,setUser] = React.useState('')
-  return (
-   <userContext.Provider value={[user,setUser]}>
-      <Header2/>
-      <Content2/>
-   </userContext.Provider> 
-  )
+// function App() {
+//   return (
+//     <Router2/>
+//   )
+// }
+
+export default function App(){
+  return <Router2/>
 }
-
-export default App;
---
+++ src/products.js
@@ -0,0 +1,12 @@
+import React  from "react";
+import './product.css'
+
+export default class Products{
+    data = [
+        ['React',500],
+        ['React Native',650],
+        ['Node.js',450],
+        ['NomgoDB',300],
+        ['Express.js',650]
+    ]
+}

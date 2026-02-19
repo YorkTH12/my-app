aa1adac648a24b6170624903463165edc61af2d7
YorkTH12 <148939113+YorkTH12@users.noreply.github.com>
2026-02-19 10:15:51 +0700

    Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
-import {userContext} from "./context"
-import Header2 from './context-header2';
-import Content2 from './context-content2'
-import Router2 from './router2'
+import Router3 from './router3';
 
 // function App() {
 //   return (
@@ -12,5 +9,5 @@ import Router2 from './router2'
 // }
 
 export default function App(){
-  return <Router2/>
+  return <Router3/>
 }
